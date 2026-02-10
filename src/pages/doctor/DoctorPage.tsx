ca201cf645707f511b0f445c4b084a702846cc71
Maksim Necheporuk <mnecheporuka@ozon.ru>
2026-02-10 22:02:38 +0300

    Фиксы

--- src/pages/doctor/DoctorPage.tsx
+++ src/pages/doctor/DoctorPage.tsx
@@ -1,34 +1,39 @@
 import classes from './doctor.module.scss'
-import { DoctorHeader } from '../../widgets/doctorHeader';
-import { DoctorCard } from '../../widgets/doctorCard';
-import { Navigate, useParams } from 'react-router-dom';
-import { HOME_ROUTE } from '../../app/router/routes';
-import { ChangeAdditionalCityDoctor, ChangeAdditionalSpecialityDoctor } from '../../widgets/changeAdditionalDoctor';
-import { ChangeSubsDoctor } from '../../widgets/changeSubsDoctor';
-import { DeleteAction } from '../../features/deleteAction';
-import { DoctorDelete } from '../../widgets/doctorDelete';
+import {DoctorHeader} from '../../widgets/doctorHeader';
+import {DoctorCard} from '../../widgets/doctorCard';
+import {Navigate, useLocation, useParams} from 'react-router-dom';
+import {HOME_ROUTE} from '../../app/router/routes';
+import {ChangeAdditionalCityDoctor, ChangeAdditionalSpecialityDoctor} from '../../widgets/changeAdditionalDoctor';
+import {ChangeSubsDoctor} from '../../widgets/changeSubsDoctor';
+import {useEffect,} from "react";
 
 
 export default function DoctorPage() {
 
-    const {id} = useParams<{id: string}>()
-
+    const {id} = useParams<{ id: string }>()
+    const {pathname} = useLocation();
     const doctorId = Number(id);
 
-    if(!doctorId){
-        return <Navigate to={HOME_ROUTE.path} />
+    useEffect(() => {
+        window.scrollTo({
+            top: 0, left: 0, behavior: 'smooth'
+        });
+    }, [pathname]);
+
+    if (!doctorId) {
+        return <Navigate to={HOME_ROUTE.path}/>
     }
 
     return (
         <section className={classes.container + " wrapper_main"}>
-            <DoctorHeader doctorId={doctorId} />
-            <DoctorCard doctorId={doctorId} />
-            <ChangeAdditionalSpecialityDoctor doctorId={doctorId} />
-            <ChangeAdditionalCityDoctor doctorId={doctorId} />
-            <ChangeSubsDoctor />
-            <section className={classes.delete}>
-                <DoctorDelete doctorId={doctorId} />
-            </section>
+            <DoctorHeader doctorId={doctorId}/>
+            <DoctorCard doctorId={doctorId}/>
+            <ChangeAdditionalSpecialityDoctor doctorId={doctorId}/>
+            <ChangeAdditionalCityDoctor doctorId={doctorId}/>
+            <ChangeSubsDoctor/>
+            {/*<section className={classes.delete}>*/}
+            {/*    <DoctorDelete doctorId={doctorId}/>*/}
+            {/*</section>*/}
         </section>
     )
 }
--- src/pages/freelancer/Freelancer.tsx
+++ src/pages/freelancer/Freelancer.tsx
@@ -1,34 +1,39 @@
 import classes from './freelancer.module.scss'
-import { Navigate, useParams } from 'react-router-dom';
-import { HOME_ROUTE } from '../../app/router/routes';
-import { FreelancerHeader } from '../../widgets/freelancerHeader';
-import { FreelancerCard } from '../../widgets/freelancerCard';
-import { ChangeAdditionalFreelancer } from '../../widgets/changeAdditionalFreelancer';
-import { ChangePriceList } from '../../widgets/changePriceList';
-import { FreelancerDelete } from '../../widgets/freelancerDelete';
-import { ChangeRecommendation } from '../../widgets/changeRecommendation';
-
+import {Navigate, useLocation, useParams} from 'react-router-dom';
+import {HOME_ROUTE} from '../../app/router/routes';
+import {FreelancerHeader} from '../../widgets/freelancerHeader';
+import {FreelancerCard} from '../../widgets/freelancerCard';
+import {ChangeAdditionalFreelancer} from '../../widgets/changeAdditionalFreelancer';
+import {ChangePriceList} from '../../widgets/changePriceList';
+import {ChangeRecommendation} from '../../widgets/changeRecommendation';
+import {useEffect,} from "react";
 
 export default function FreelancerPage() {
 
-    const {id} = useParams<{id: string}>()
-
+    const {id} = useParams<{ id: string }>()
+    const {pathname} = useLocation();
     const freelancerId = Number(id);
 
-    if(!freelancerId){
-        return <Navigate to={HOME_ROUTE.path} />
+    useEffect(() => {
+        window.scrollTo({
+            top: 0, left: 0, behavior: 'smooth'
+        });
+    }, [pathname]);
+
+    if (!freelancerId) {
+        return <Navigate to={HOME_ROUTE.path}/>
     }
 
     return (
         <section className={classes.container + " wrapper_main"}>
-            <FreelancerHeader freelancerId={freelancerId} />
-            <FreelancerCard freelancerId={freelancerId} />
-            <ChangeAdditionalFreelancer freelancerId={freelancerId} />
-            <ChangePriceList freelancerId={freelancerId} />
-            <ChangeRecommendation freelancerId={freelancerId} />
-            <section className={classes.delete}>
-                <FreelancerDelete freelancerId={freelancerId} />
-            </section>
+            <FreelancerHeader freelancerId={freelancerId}/>
+            <FreelancerCard freelancerId={freelancerId}/>
+            <ChangeAdditionalFreelancer freelancerId={freelancerId}/>
+            <ChangePriceList freelancerId={freelancerId}/>
+            <ChangeRecommendation freelancerId={freelancerId}/>
+            {/*<section className={classes.delete}>*/}
+            {/*    <FreelancerDelete freelancerId={freelancerId} />*/}
+            {/*</section>*/}
         </section>
     )
 }
